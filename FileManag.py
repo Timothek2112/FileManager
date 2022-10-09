b2e3d5de108fc5ec6050ac0b535b1a1ef1df89b3
timothek <nikzav6080@gmail.com>
2022-10-09 18:48:42 +0700

    first commit

--- FileManag.py
+++ FileManag.py
@@ -1,23 +1,41 @@
 from genericpath import isfile
 
+class MessageHaveNumbers(Exception):
+    def __init__(self, message="FIO can't include non-alphabetic simbols"):
+        self.message = message
+        super().__init__(self.message)
+        
+class PhoneHaveNonNumeric(Exception):
+    def __init__(self, message = "Phone number cannot include non-numeric simbols"):
+        self.message = message
+        super().__init__(self.message)
+        
 
 class FileManager:
     def create(self, name:str):
-        if isfile(name):
-            return "File already exists"
-        else:
-            file = open(name, "w+")
-            file.close()
-            return "file" + name + "was succefully created"
+        try:
+            if isfile(name):
+                return "File already exists"
+            else:
+                file = open(name, "w+")
+                file.close()
+                return "file " + name + " was succefully created"
+        except FileNotFoundError as e:
+            print(e)
+            return "File is not exist"
+
     def write(self,name:str,message:str):
         if isfile(name):
             file = open(name,"a+")
             file.write("\n" + message)
+            file.close()
+            file = open(name,"r")
             r = file.read()
             file.close()
             return r
         else:
             return "This file dont exist"
+
     def read(self,name:str):
         if isfile(name):
             file = open(name,"r")
--- main.py
+++ main.py
@@ -1,9 +1,50 @@
-from venv import create
-from FileManag import FileManager
+from curses.ascii import isalnum
+from email.message import Message
+from FileManag import FileManager, MessageHaveNumbers, PhoneHaveNonNumeric
 
-while True:
+fm = FileManager
+def checkIfNumeric(message:str):
+    if message.isalnum():
+        pass
+    else:
+        raise PhoneHaveNonNumeric
+
+def checkIfAlphabetic(message:str):
+    if message.isalpha():
+        pass
+    else:
+        raise MessageHaveNumbers
+
+while True:    
     mode = input("Choose a mode: ")
-    if mode == "c":
-        name = input("Choose a file's name: ")
-        fm = FileManager
-        FileManager.create(fm,name)
+
+    if mode == "w" or mode == "r":
+        name = input("Choose a file: ")
+        name += ".txt"
+        if mode == 'r':
+            print(FileManager.read(fm,name))
+        elif mode == 'w':
+            try:
+                F = input("Insert a surname: ")
+                checkIfAlphabetic(F)
+                I = input("Insert a name: ")
+                checkIfAlphabetic(I)
+                O = input("Insert a second name: ")
+                checkIfAlphabetic(O)
+                FIO = F + " " + I + " " + O
+                
+                phone = input("Insert phone number: ")
+                checkIfNumeric(phone)
+                print(FileManager.write(fm,name,FIO))
+
+            except MessageHaveNumbers as e:
+                print(e)
+            except PhoneHaveNonNumeric as e:
+                print(e)
+    else:
+        if mode == "c":
+            name = input("Choose a file's name: ")
+            fm = FileManager
+            print(FileManager.create(fm,name + ".txt"))
+    
+    
